4c4e36366eae4d18671d9abc62076bfca67ff2cd
Govind <govinddhingra@hotmail.com>
2021-09-02 22:46:58 +0530

    ci cd

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Scribe app is running!');
+    expect(page.getTitleText()).toEqual('Perspective app is running!');
   });
 
   afterEach(async () => {
